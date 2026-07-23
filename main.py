97e49f9596c420124a6bf195ecc34019397fba09
Sumit Lasiwa <lasiwasumit@gmail.com>
2026-07-23 19:09:31 +0545

    Stage 3: create with validation

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI, status, HTTPException
+from pydantic import BaseModel
+from random import randrange
 
 app = FastAPI()
 
@@ -8,6 +10,11 @@ tasks = [
     {"id": 3, "title": "upper body workout", "done": False }
 ]
 
+class Input(BaseModel):
+    title: str
+    done: bool = False
+
+    
 @app.get("/")
 async def root():
     return { "name": "Task API", "version": "1.0", "endpoints": ["/tasks"] }
@@ -15,6 +22,17 @@ async def root():
 @app.get("/health")
 def health_check():
     return { "status": "ok"}
+
+@app.post("/tasks", status_code=status.HTTP_201_CREATED)
+def create_task(input: Input):
+
+    if not input.title:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="title should not be empty!")
+    
+    input_dict = input.model_dump()
+    input_dict["id"] = randrange(0, 1000000)
+    tasks.append(input_dict)
+    return {"data": input_dict}
 
 @app.get("/tasks")
 def get_tasks():
